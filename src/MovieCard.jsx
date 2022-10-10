00c94550fc4b4d504b41ff213d6e35008152119a
rihan_a <a.rihan@live.com>
2022-10-10 21:10:07 +0200

    changes in the jsx arrangment for style reasons

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -6,22 +6,20 @@ const MovieCard = ({ movie }) => {
 
     return (
         <div className="movie" onClick={() => window.open(imdbUrl, "_blank")}>
-            <div>
-                <p>{movie.Year}</p>
-            </div>
-            <div>
+            <div className="movie-poster">
                 <img
                     src={
                         movie.Poster !== "N/A"
                             ? movie.Poster
-                            : "https://via,placeholder.com/400"
+                            : "https://via.placeholder.com/400"
                     }
                     alt={movie.Title}
                 />
             </div>
-            <div>
+            <div className="movie-desc">
                 <span>{movie.Type}</span>
                 <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
             </div>
         </div>
     );
